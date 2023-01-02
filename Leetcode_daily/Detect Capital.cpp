87e8eda805e1240bde0a3061f7346d1d08fb3aa0
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2023-01-02 12:21:47 +0530

    Update Detect Capital.cpp

--- Leetcode_daily/Detect Capital.cpp
+++ Leetcode_daily/Detect Capital.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-    return all_of(word.begin(), word.end(), [](unsigned char ch) {return isupper(ch);}) ||
-        all_of(word.begin(), word.end(), [](unsigned char ch) {return islower(ch);}) ||
-        isupper(word[0]) && all_of(word.begin() + 1, word.end(), [](unsigned char ch) {return islower(ch);});
+    return all_of(word.begin(), word.end(), ::isupper) ||
+        all_of(word.begin()+1, word.end(), ::islower);
 }
 };
